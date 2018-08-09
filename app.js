b5c3fbef7b5563d29c3b4d08a956321fd355e16b
Anthony Geiger <ageiger@silversphere.com>
2018-08-09 03:07:08 +0000

    app starting, landing page created as test

--- app.js
+++ app.js
@@ -11,7 +11,12 @@ var express                 = require("express"),
 // passport config
 
 // use routes
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
 
+app.get("/", function(req, res){
+   res.render("landing");
+});
 // start server
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The Eva Wearable Feedback Submission Form server has been started!");
